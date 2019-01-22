084c55cff6629a66729fa763850cb5fe31017b42
Keaton Muhlestein <keatonmuhlestein@mail.weber.edu>
2019-01-22 09:04:59 -0700

    Initial Commit

---
+++ mod3/chars.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  chars.c
+ *
+ *    Description:  learn how to use characters
+ *
+ *        Version:  1.0
+ *        Created:  01/22/2019 08:53:38 AM
+ *       Revision:  none
+ *       Compiler:  gcc chars.c -o chars.out
+ *
+ *         Author:  Keaton Muhlestein (), keatonmuhlestein@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    int age = 20;
+    double weight = 178.3;
+    char sex = 'M';
+
+    printf("Your age is %d and your weight %lf and your sex is %c\n",
+            age, weight, sex);
+
+    return 0;
+}
+// Function Definitions
+
+
