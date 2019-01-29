c3a9c70887eabc47e266a53d52620f5c9c06d6d3
Keaton Muhlestein <keatonmuhlestein@mail.weber.edu>
2019-01-29 10:08:41 -0700

    Use switch statement

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -23,12 +23,16 @@
 #define FREEZING_F 32
 #define BOIL_C 100
 #define BOIL_F 212
+#define BOIL 1
+#define LIQUID 2
+#define ICE 3
 // Main Function
 int main()
 {
   //  float temp = 7.9;
     double in_temp, out_temp;
     int choice;
+    int water_state = 0;
     //Ask user which temperature mode they want to use
     printf("Welcome to the temperature calculator\n");
     printf("Please enter your choice:\n");
@@ -59,33 +63,44 @@ int main()
     {
         if (in_temp >= BOIL_C)
         {
-            printf("Your water is boiling\n");
+            water_state = BOIL;
         }
         else if (in_temp > FREEZING_C && in_temp < BOIL_C)
         {
-            printf("Your water is liquid\n");
+            water_state = LIQUID;
         }
         else 
         {
-            printf("Your water is freezing\n");
+            water_state = ICE;
         }
     }
     else
     {
         if (in_temp >= BOIL_F)
         {
-            printf("Your water is boiling\n");
+            water_state = BOIL;
         }
         else if (in_temp > FREEZING_F && in_temp < BOIL_F)
         {
-            printf("Your is liquid\n");
+            water_state = LIQUID;
         }
-        else 
+        else
         {
-            printf("Your water is freezing\n");
+            water_state = ICE;
         }
     }
-
+    switch (water_state)
+    {
+        case BOIL:
+            printf("Your water is boiling\n");
+            break;
+        case LIQUID:
+            printf("Your is liquid\n");
+            break;
+        case ICE:
+            printf("Your water is freezing\n");
+            break;
+    }
 
     return 0;
 }
