79d408b909765b6c596ceea086246d48c27ab933
Keaton Muhlestein <keatonmuhlestein@mail.weber.edu>
2019-01-29 09:14:08 -0700

    Changed to choices

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -1,14 +1,14 @@
 /*
  * =====================================================================================
  *
- *       Filename:  float.c
+ *       Filename:  temperature.c
  *
  *    Description:  Play with float points.
  *
  *        Version:  1.0
  *        Created:  01/15/2019 09:26:46 AM
  *       Revision:  none
- *       Compiler:  gcc float.c -o float
+ *       Compiler:  gcc temperature.c -o temperature
  *
  *         Author:  Keaton Muhlestein (), keatonmuhlestein@mail.weber.edu
  *   Organization:  WSU
@@ -17,19 +17,43 @@
  */
 #include <stdio.h>
 
-
+#define CELSIUS 0
+#define FAHRENHEIT 1
 // Main Function
 int main()
 {
-    float temp = 7.9;
-    double f_temp = 0;
-    printf("The temperature is %f Celsius\n", temp);
-    printf("What is the temperature in Ogden now in Celsius: ");
-    scanf("%f", &temp);
-    f_temp = (temp * 9/5) + 32;
-    printf("I see, your temp is %lf in Fahrenheit.\n", f_temp);
-    
-    return 0;
+  //  float temp = 7.9;
+    double in_temp, out_temp;
+    int choice;
+    //Ask user which temperature mode they want to use
+    printf("Welcome to the temperature calculator\n");
+    printf("Please enter your choice:\n");
+    printf("\t0 for Celsius or 1 for Fahrenheit: ");
+    scanf("%d", &choice);
+    if (choice == 0)
+    {
+        printf("Welcome to the Celsius to Fahrenheit calculator\n");
+        printf("Enter the temperature in Celsius: ");
+        scanf("%lf", &in_temp);
+        out_temp = (in_temp * 9.0/5.0) + 32;
+        printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
+    }
+    else if (choice == 1)
+    {
+        printf("Welcome to the Fahrenheit to Celsius calculator\n");
+        printf("Enter the temperature in Fahrenheit: ");
+        scanf("%lf", &in_temp);
+        out_temp = (in_temp - 32) * 5.0/9.0;
+        printf("Your temperature in Celsius is: %lf\n", out_temp);
+    }
+    else
+    {
+        printf("Sorry, you did not enter 0 or 1\n Adios amigo\n");
+        return 0;
+    }
+
+
+        return 0;
 }
 // Function Definitions
 
