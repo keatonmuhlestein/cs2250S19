733fcada66cbd55d17de965132f0ef624e9dc35e
Keaton Muhlestein <keatonmuhlestein@mail.weber.edu>
2019-01-29 09:46:56 -0700

    Determine physical state of matter

--- mod4/temperature.c
+++ mod4/temperature.c
@@ -19,6 +19,10 @@
 
 #define CELSIUS 0
 #define FAHRENHEIT 1
+#define FREEZING_C 0
+#define FREEZING_F 32
+#define BOIL_C 100
+#define BOIL_F 212
 // Main Function
 int main()
 {
@@ -36,7 +40,7 @@ int main()
         printf("Enter the temperature in Celsius: ");
         scanf("%lf", &in_temp);
         out_temp = (in_temp * 9.0/5.0) + 32;
-        printf("Your temperature in Fahrenheit is: %lf\n", out_temp);
+        printf("Your temperature in Fahrenheit is: %.2lf\n", out_temp);
     }
     else if (choice == FAHRENHEIT)
     {
@@ -44,16 +48,46 @@ int main()
         printf("Enter the temperature in Fahrenheit: ");
         scanf("%lf", &in_temp);
         out_temp = (in_temp - 32) * 5.0/9.0;
-        printf("Your temperature in Celsius is: %lf\n", out_temp);
+        printf("Your temperature in Celsius is: %.2lf\n", out_temp);
     }
     else
     {
         printf("Sorry, you did not enter 0 or 1\n Adios amigo\n");
         return 0;
     }
+    if (choice == CELSIUS)
+    {
+        if (in_temp >= BOIL_C)
+        {
+            printf("Your water is boiling\n");
+        }
+        else if (in_temp > FREEZING_C && in_temp < BOIL_C)
+        {
+            printf("Your water is liquid\n");
+        }
+        else 
+        {
+            printf("Your water is freezing\n");
+        }
+    }
+    else
+    {
+        if (in_temp >= BOIL_F)
+        {
+            printf("Your water is boiling\n");
+        }
+        else if (in_temp > FREEZING_F && in_temp < BOIL_F)
+        {
+            printf("Your is liquid\n");
+        }
+        else 
+        {
+            printf("Your water is freezing\n");
+        }
+    }
 
 
-        return 0;
+    return 0;
 }
 // Function Definitions
 
